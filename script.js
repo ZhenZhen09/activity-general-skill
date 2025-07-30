234b74d4496e648743a50b1f9cd5a286730d604b
ZhenZhen09 <125789160+ZhenZhen09@users.noreply.github.com>
2025-07-30 12:53:46 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const encodedString = "Vm0weGQxTnRVWGxXYTFwUFZsZG9WRmxVU2xOalJsSlZWR3RPYW1KR1ZqTldNakZIWVZVeFYxTnNXbFpXTTFGM1ZrUktTMUl4WkhOaFJtUnBWMFZLUlZkV1VrZFpWMDV6V2toT2FsSnVRazlWYlhoM1ZGWmFjVk5xVWxoaVZrWXpWR3hXYzFsV1NuTlRiR2hhWWtkU2RsbHFSbXRXVmtaMFpFWmFUbFpVVmtsV2Fra3hWVEpHUjFOcVdsTmlSMUpZV1ZSR1MxRXhVbk5TVkd4UlZWUXdPUT09";
-
-const decodedString = atob(encodedString);
-
-const repeatedString = decodedString.repeat(8);
-
-console.log(repeatedString);
